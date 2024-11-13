522b2a427416d47838f24b303839a14ed45c7f29
Mathieu Dalbin <mathieu.dalbin@fr.ibm.com>
2024-11-13 15:06:49 +0100

    Update Setup.sh
    Co-authored-by: Dennis Behm <dennis.behm@de.ibm.com>

--- Setup.sh
+++ Setup.sh
@@ -84,7 +84,7 @@ PUBLISH_ARTIFACTS=true
 ARTIFACT_REPOSITORY_SERVER_URL=http://10.3.20.231:8081/artifactory
 # User to connect to the Artifact Repository Server
 # e.q.:  ARTIFACT_REPOSITORY_USER=admin
-ARTIFACT_REPOSITORY_USER=admin
+ARTIFACT_REPOSITORY_USER=user
 # Password to connect to the Artifact Repository Server
 # e.q.: ARTIFACT_REPOSITORY_PASSWORD=xxxxx
 ARTIFACT_REPOSITORY_PASSWORD=artifactoryadmin
