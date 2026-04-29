29c329af40f5f2dfb20172da2937d298616a5ff7
Mathieu Dalbin <mathieu.dalbin@fr.ibm.com>
2026-04-29 14:23:07 +0200

    Removed File Metadatastore if used and exists

--- src/scripts/Migration-Modeler-Start.sh
+++ src/scripts/Migration-Modeler-Start.sh
@@ -119,6 +119,12 @@ if [ $rc -eq 0 ]; then
             rm -rf $DBB_MODELER_BUILD_CONFIGURATION
             echo "[INFO] Removed '${DBB_MODELER_BUILD_CONFIGURATION}' folder"
         fi
+		if [ "$DBB_MODELER_METADATASTORE_TYPE" == "file"] ;
+			if [ -d $DBB_MODELER_FILE_METADATA_STORE_DIR ]; then
+				rm -rf $DBB_MODELER_FILE_METADATA_STORE_DIR
+				echo "[INFO] Removed '${DBB_MODELER_FILE_METADATA_STORE_DIR}' folder"
+			fi
+		fi
 	fi
 fi
 
