694e15b1141d83a0c27acfe7e91149df49d5a1a6
Mathieu Dalbin <mathieu.dalbin@fr.ibm.com>
2026-09-10 15:39:31 +0200

    Added quotes

--- src/java/com/ibm/dbb/migration/UpdateZBuilderConfiguration.java
+++ src/java/com/ibm/dbb/migration/UpdateZBuilderConfiguration.java
@@ -101,23 +101,23 @@ public class UpdateZBuilderConfiguration {
         String metadataStoreType = props.getProperty("DBB_MODELER_METADATASTORE_TYPE");
         Map<String, String> typeVar = new LinkedHashMap<>();
         typeVar.put("name", "type");
-        typeVar.put("value", metadataStoreType);
+        typeVar.put("value", "\"" + metadataStoreType + "\"");
         variables.add(typeVar);
 
         if ("file".equals(metadataStoreType)) {
             Map<String, String> locationVar = new LinkedHashMap<>();
             locationVar.put("name", "fileLocation");
-            locationVar.put("value", props.getProperty("DBB_MODELER_FILE_METADATA_STORE_DIR"));
+            locationVar.put("value", "\"" + props.getProperty("DBB_MODELER_FILE_METADATA_STORE_DIR") + "\"");
             variables.add(locationVar);
         } else if ("db2".equals(metadataStoreType)) {
             Map<String, String> db2UrlVar = new LinkedHashMap<>();
             db2UrlVar.put("name", "db2Url");
-            db2UrlVar.put("value", props.getProperty("DBB_MODELER_DB2_URL"));
+            db2UrlVar.put("value", "\"" + props.getProperty("DBB_MODELER_DB2_URL") + "\"");
             variables.add(db2UrlVar);
 
             Map<String, String> db2ConfVar = new LinkedHashMap<>();
             db2ConfVar.put("name", "db2Conf");
-            db2ConfVar.put("value", props.getProperty("DBB_MODELER_DB2_METADATASTORE_CONFIG_FILE"));
+            db2ConfVar.put("value", "\"" + props.getProperty("DBB_MODELER_DB2_METADATASTORE_CONFIG_FILE") + "\"");
             variables.add(db2ConfVar);
         }
 
